d0ee5508ef85aff1703dc0771729dc38854f9886
hasanaly <hsn_aly@hotmail.com>
2022-12-14 16:55:26 +0300

    feat: add source

---
+++ App/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ App/Controllers/RingtoneController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class RingtoneController : Controller
+    {
+        public IActionResult Index()
+        {
+            //TODO: Implement Realistic Implementation
+            return View();
+        
+        }
+
+        public IActionResult Create()
+        {
+            //TODO: Implement Realistic Implementation
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            //TODO: Implement Realistic Implementation
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            //TODO: Implement Realistic Implementation
+            return View();
+        }
+    }
+}
