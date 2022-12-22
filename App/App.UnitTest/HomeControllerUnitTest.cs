060f85901f46cd9422592f2ed3c466b2e8476595
hasanaly <hsn_aly@hotmail.com>
2022-12-22 20:42:05 +0300

    feat : add unit tests for details and contact

--- App/App.UnitTest/HomeControllerUnitTest.cs
+++ App/App.UnitTest/HomeControllerUnitTest.cs
@@ -1,4 +1,6 @@
 using App.Controllers;
+using App.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace App.UnitTest;
 
@@ -43,6 +45,79 @@ public class HomeControllerUnitTest
 
         // Assert
         Assert.IsInstanceOf<NotFoundResult>(result);
+    }
+    
+    [Test]
+    public void Index_ReturnsViewResult_WithListOfRingtones_WithCorrectData()
+    {
+        // Arrange
+        var controller = new HomeController();
+
+        // Act
+        var result = controller.Index();
+
+        // Assert
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = result as ViewResult;
+        Assert.IsInstanceOf<List<Ringtone>>(viewResult.Model);
+        var model = viewResult.Model as List<Ringtone>;
+        Assert.AreEqual(2, model.Count);
+        Assert.AreEqual("Ringtone 1", model[0].Name);
+        Assert.AreEqual("Ringtone 2", model[1].Name);
+    }
+    
+    [Test] 
+    public void Details_ReturnsNotFound_ForInvalidId()
+    {
+        // Arrange
+        var controller = new HomeController();
+
+        // Act
+        var result = controller.Details(0);
+
+        // Assert
+        Assert.IsInstanceOf<NotFoundResult>(result);
+    }
+    
+    [Test] 
+    public void Details_ReturnsViewResult_WithRingtone()
+    {
+        // Arrange
+        var controller = new HomeController();
+
+        // Act
+        var result = controller.Details(1);
+
+        // Assert
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = result as ViewResult;
+        Assert.IsInstanceOf<Ringtone>(viewResult.Model);
+    }
+    
+    [Test]
+    public void Contact()
+    {
+        // Arrange
+        var controller = new HomeController();
+
+        // Act
+        var result = controller.Contact();
+
+        // Assert
+        Assert.IsNotNull(result);
+    }
+    
+    [Test]
+    public void Contact_ReturnsViewResult()
+    {
+        // Arrange
+        var controller = new HomeController();
+
+        // Act
+        var result = controller.Contact();
+
+        // Assert
+        Assert.IsInstanceOf<ViewResult>(result);
     }
 
 }
