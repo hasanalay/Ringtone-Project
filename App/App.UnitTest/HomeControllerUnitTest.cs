0d9b7a8080f22d6c3618ba55056d457abdbadc17
hasanaly <hsn_aly@hotmail.com>
2022-12-22 20:36:54 +0300

    feat: add Index_ReturnsViewResult_WithListOfRingtones & Index_ReturnsNotFound_ForEmptyDatabase

--- App/App.UnitTest/HomeControllerUnitTest.cs
+++ App/App.UnitTest/HomeControllerUnitTest.cs
@@ -16,4 +16,33 @@ public class HomeControllerUnitTest
         // Assert
         Assert.IsNotNull(result);
     }
+    
+    [Test]
+    public void Index_ReturnsViewResult_WithListOfRingtones()
+    {
+        // Arrange
+        var controller = new HomeController();
+
+        // Act
+        var result = controller.Index();
+
+        // Assert
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = result as ViewResult;
+        Assert.IsInstanceOf<List<Ringtone>>(viewResult.Model);
+    }
+    
+    [Test]
+    public void Index_ReturnsNotFound_ForEmptyDatabase()
+    {
+        // Arrange
+        var controller = new HomeController();
+
+        // Act
+        var result = controller.Index();
+
+        // Assert
+        Assert.IsInstanceOf<NotFoundResult>(result);
+    }
+
 }
