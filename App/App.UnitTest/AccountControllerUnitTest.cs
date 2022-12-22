f9e22c75031b1bcb56130d412eeff8631654351e
hasanaly <hsn_aly@hotmail.com>
2022-12-22 20:20:10 +0300

    feat: add TestLogoutAction and TestLogoutActionRedirectsToLogin unit tests

--- App/App.UnitTest/AccountControllerUnitTest.cs
+++ App/App.UnitTest/AccountControllerUnitTest.cs
@@ -35,7 +35,34 @@ public class AccountControllerTest
         // Assert
         Assert.That(result, Is.TypeOf<ViewResult>());
     }
+    
+    [Test]
+    public void TestLogoutAction()
+    {
+        // Arrange
+        var mock = new Mock<IAccountService>();
+        var controller = new AccountController(mock.Object);
 
+        // Act
+        var result = controller.Logout();
+
+        // Assert
+        Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+    }
+    
+    [Test]
+    public void TestLogoutActionRedirectsToLogin()
+    {
+        // Arrange
+        var mock = new Mock<IAccountService>();
+        var controller = new AccountController(mock.Object);
+
+        // Act
+        var result = controller.Logout() as RedirectToActionResult;
+
+        // Assert
+        Assert.That(result.ActionName, Is.EqualTo("Login"));
+    }
 }
 
 public interface IAccountService
