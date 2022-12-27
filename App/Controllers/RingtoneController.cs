5ddf5cc29d896520adf4dd6c43cffcd7dd86d2f4
Simay Ozge Serce <ozgesimsercezgesimserce>
2022-12-27 20:00:15 +0300

    Add search function

--- App/Controllers/RingtoneController.cs
+++ App/Controllers/RingtoneController.cs
@@ -1,10 +1,16 @@
 using App.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace App.Controllers
 {
     public class RingtoneController : Controller
     {
+        private DbProjectContext db { get; }
+        public RingtoneController(DbProjectContext _context)
+        {
+            this.db = _context;
+        }
         public IActionResult Index()
         {
             //TODO: Implement Realistic Implementation
@@ -29,7 +35,14 @@ namespace App.Controllers
             //TODO: Implement Realistic Implementation
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Search(string searchedWord)
+        {
+            //ViewData["Details"] = searchedWord;
+            var searched = from x in db.Ringtones select x;
+            searched = searched.Where(x => x.Name.Contains(searchedWord) || x.Details.Contains(searchedWord));
+            return View(searched);
+        }
 
     }
 }
